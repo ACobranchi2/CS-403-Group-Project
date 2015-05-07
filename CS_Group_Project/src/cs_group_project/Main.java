abb1551fda85a463ac26aa4a04f84ca9e03b171c
vashirick <sdement@wvup.edu>
2015-05-07 18:50:06 -0400

    server/client stuff
    These are the files That i found that make the server and client parts
    we don't have.  Some need changed a bit to fit our code, been trying to
    make things work but hasn't been going my way.

---
+++ CS_Group_Project/src/cs_group_project/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public class Main {
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        // TODO code application logic here
+        new GameStarterUI(new Game());
+    }
+
+}
---
+++ CS_Group_Project/src/cs_group_project/ServerMain.java
@@ -0,0 +1,8 @@
+public class ServerMain {
+
+    public static void main(String[] args) {
+            // TODO code application logic here
+        new Server(10000, 2);
+    }
+
+}
